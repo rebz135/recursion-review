c254e99d775985da279ad4290cee5d2737024954
rebz135 <rebz135@gmail.com>
2018-07-30 11:27:39 -0700

    added psuedocode for how to solve.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,17 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // take in primative, array, obj, string, nested arrays/ objects
+    // determine type
+      // if primative return string
+      // if array, loop through elements
+      // add '['
+        // if array or obj run stringify again with element as input
+      // add ']'
+      // if obj add '{', loop through key value pairs
+        // if value is array or obj, run stringify with value
+      // add '}'
+  // return string
+
+  return result;
 };
